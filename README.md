31894808006fa6a3f408d5f88fcb0e9afba2081f
CatLooks <catlooksoff@gmail.com>
2025-10-04 21:04:29 +0200

    test window

---
+++ README.md
@@ -0,0 +1,8 @@
+# One of games of all time
+
+*(add description later)*
+
+## Dependencies
+
+* Language: C++17
+* Libraries: SFML 3.0
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// include SFML
+#include <SFML/Graphics.hpp>
+
+/// Program entry.
+/// @return Exit code.
+int main() {
+	// test window
+	sf::RenderWindow win(sf::VideoMode({ 1600, 900 }), "App");
+	win.setVerticalSyncEnabled(true);
+	while (win.isOpen()) {
+		while (const auto event = win.pollEvent()) {
+			if (event->is<sf::Event::Closed>())
+				win.close();
+		};
+		win.clear(sf::Color(29, 31, 37));
+		win.display();
+	};
+};
