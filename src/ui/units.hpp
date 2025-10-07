767f802d3d910b7ac9c059929faf1ba4de6bef20
CatLooks <catlooksoff@gmail.com>
2025-10-07 17:26:32 +0200

    added dimension vector & rectangle

--- src/ui/units.hpp
+++ src/ui/units.hpp
@@ -2,9 +2,9 @@
 
 // include dependencies
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Rect.hpp>
 
 namespace ui {
-
 	/// Linearly interpolates between 2 integers.
 	/// 
 	/// @param a Starting integer.
@@ -31,12 +31,12 @@ namespace ui {
 		float ps; /// Parent size scalar.
 		float es; /// Element size scalar.
 
-		/// Converts dimension into its true position.
+		/// Returns dimension's true value.
 		/// 
 		/// @param parent_size Size of parent element.
 		/// @param element_size Size of the element.
 		/// 
-		/// @return Offset in pixels.
+		/// @return Offset in pixels from parent origin.
 		int get(int parent_size, int element_size) const {
 			return px + (int)(ps * parent_size + es * element_size);
 		};
@@ -59,9 +59,9 @@ namespace ui {
 		Dim operator+(const Dim& oth) const { return { px + oth.px, ps + oth.ps, es + oth.es }; };
 		/// Subtracts 2 dimensions.
 		Dim operator-(const Dim& oth) const { return { px - oth.px, ps - oth.ps, es - oth.es }; };
-		/// Scale a dimension by a factor.
-		Dim operator*(float scale) { return { (int)(px * scale), ps * scale, es * scale }; };
-		/// Inverts a dimension.
+		/// Scale the dimension by a factor.
+		Dim operator*(float scale) const { return { (int)(px * scale), ps * scale, es * scale }; };
+		/// Inverts the dimension.
 		Dim operator-() const { return { -px, -ps, -es }; };
 
 		/// Linearly interpolates between 2 dimensions.
@@ -73,11 +73,94 @@ namespace ui {
 		/// @return Interpolated dimension.
 		static Dim lerp(const Dim& a, const Dim& b, float t) { return a + (b - a) * t; };
 	};
+
+	/// Dimension 2D vector.
+	/// Uses axis dimensions to store position or size.
+	struct DimVector {
+		Dim x; /// "X" axis dimension.
+		Dim y; /// "Y" axis dimension.
+
+		/// Returns dimension vector's true value.
+		/// 
+		/// @param parent Size of parent element.
+		/// @param element Size of the element.
+		/// 
+		/// @return Offset vector from parent origin.
+		sf::Vector2i get(sf::Vector2i parent, sf::Vector2i element) const {
+			return { x.get(parent.x, element.x), y.get(parent.y, element.y) };
+		};
+
+		/// Constructs a vector pointing to origin (0, 0).
+		DimVector() {};
+		/// Constructs a vector from specified values.
+		/// 
+		/// @param x "X" axis dimension.
+		/// @param y "Y" axis dimension.
+		DimVector(Dim x, Dim y) : x(x), y(y) {};
+		/// Constructs a vector from an integer vector.
+		///
+		/// @param vec Integer vector.
+		DimVector(sf::Vector2i vec) : x(vec.x), y(vec.y) {};
+
+		/// Adds 2 dimension vectors.
+		DimVector operator+(const DimVector& oth) const { return { x + oth.x, y + oth.y }; };
+		/// Subtracts 2 dimension vectors.
+		DimVector operator-(const DimVector& oth) const { return { x - oth.x, y - oth.y }; };
+		/// Scale the vector by a factor.
+		DimVector operator*(float scale) const { return { x * scale, y * scale }; };
+		/// Inverts the vector.
+		DimVector operator-() const { return { -x, -y }; };
+
+		/// Linearly interpolates between 2 dimensions.
+		/// 
+		/// @param a Starting dimension.
+		/// @param b Ending dimension.
+		/// @param t Interpolation progress.
+		/// 
+		/// @return Interpolated dimension.
+		static DimVector lerp(const DimVector& a, const DimVector& b, float t) { return a + (b - a) * t; };
+	};
+
+	/// Dimension AABB (axis aligned bounding box).
+	/// Used axis dimensions to store position and size of a rectangle.
+	struct DimRect {
+		DimVector position;  /// Rectangle position (top-left corner).
+		DimVector size;      /// Rectangle size (`size.es` will be ignored).
+
+		/// Returns dimension rectangle's true value.
+		/// 
+		/// @param parent Parent element bounding box.
+		/// 
+		/// @return Recalculated bounding box.
+		sf::IntRect get(sf::IntRect parent) const {
+			sf::Vector2i _size = size.get(parent.size, {});
+			return { position.get(parent.size, _size) + parent.position, _size };
+		};
+
+		/// Constructs an empty rectangle.
+		DimRect() {};
+		/// Constructs a rectangle from vectors.
+		/// 
+		/// @param position Rectangle position.
+		/// @param size Rectangle size.
+		DimRect(DimVector position, DimVector size) : position(position), size(size) {};
+		/// Constructs a rectangle from singular dimensions.
+		/// 
+		/// @param x Rectangle X position.
+		/// @param y Rectangle Y position.
+		/// @param width Rectangle width.
+		/// @param height Rectangle height.
+		DimRect(Dim x, Dim y, Dim width, Dim height) : position({ x, y }), size({ width, height }) {};
+		/// Constructs a rectangle from an integer rectangle.
+		/// 
+		/// @param rect Integer rectangle.
+		DimRect(sf::IntRect rect) : DimRect(rect.position.x, rect.position.y, rect.size.x, rect.size.y) {};
+	};
 };
 
 /// Converts a number into a dimension.
 ui::Dim operator""px(unsigned long long i) { return ui::Dim::from_px((int)i); };
 /// Converts a number into a dimension.
-ui::Dim operator""ps(long double f) { return ui::Dim::from_ps(f); };
+ui::Dim operator""ps(long double f) { return ui::Dim::from_ps((float)f); };
 /// Converts a number into a dimension.
-ui::Dim operator""es(long double f) { return ui::Dim::from_es(f); };
+ui::Dim operator""es(long double f) { return ui::Dim::from_es((float)f); };
