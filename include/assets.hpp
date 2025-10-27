f6124d535b03ba19cce6e922084b4719a41c82eb
CatLooks <catlooksoff@gmail.com>
2025-10-27 15:51:42 +0100

    removed test texture

--- include/assets.hpp
+++ include/assets.hpp
@@ -7,12 +7,8 @@ namespace assets {
 	/// Default game font.
 	sf::Font font;
 
-	/// Test texture
-	sf::Texture tex;
-
 	/// Loads all assets.
 	void loadAssets() {
 		loadFont("font.ttf", font);
-		loadTexture("panel.png", tex);
 	};
 };
