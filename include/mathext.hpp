d37221b4cfe78cdb6b82e72c177f431479031308
CatLooks <catlooksoff@gmail.com>
2025-11-17 15:20:36 +0100

    math helper functions

---
+++ include/mathext.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+// include dependencies
+#include "math.h"
+
+/// Math extension function namespace.
+namespace ext {
+	/// Returns `a / d` rounded down.
+	///
+	/// @param a Divident.
+	/// @param d Divisor.
+	///
+	/// @return Proper quotient.
+	int idiv(int a, int d);
+
+	/// Returns `a % b` rounded down.
+	///
+	/// @param a Divident.
+	/// @param d Divisor.
+	///
+	/// @return Proper remainder ("modulo").
+	int imod(int a, int d);
+
+	/// Calculates quotient and remainder rounded down.
+	///
+	/// @param a Divident.
+	/// @param d Divisor.
+	/// @param q Quotient.
+	/// @param r Remainder.
+	void idivmod(int a, int d, int& q, int& r);
+
+	/// Returns smaller integer.
+	int imin(int a, int b);
+	/// Returns greater integer.
+	int imax(int a, int b);
+	/// Clamps an integer to an inclusive range.
+	///
+	/// @param x Clamped value.
+	/// @param lower Lower bound.
+	/// @param upper Upper bound.
+	int iclamp(int x, int lower, int upper);
+
+	/// Returns smaller float.
+	float fmin(float a, float b);
+	/// Returns greater float.
+	float fmax(float a, float b);
+	/// Clamps a float to an inclusive range.
+	///
+	/// @param x Clamped value.
+	/// @param lower Lower bound.
+	/// @param upper Upper bound.
+	float fclamp(float x, float lower, float upper);
+};
---
+++ src/mathext.cpp
@@ -0,0 +1,47 @@
+#include "mathext.hpp"
+
+namespace ext {
+	/// Returns `a / d` rounded down.
+	int idiv(int a, int d) {
+		return (int)floorf((float)a / d);
+	};
+
+	/// Returns `a % b` rounded down.
+	int imod(int a, int d) {
+		int r = a % d;
+		if (r < 0) r += d;
+		return r;
+	};
+
+	/// Calculates quotient and remainder rounded down.
+	void idivmod(int a, int d, int& q, int& r) {
+		q = idiv(a, d);
+		r = imod(a, d);
+	};
+
+	/// Returns smaller integer.
+	int imin(int a, int b) {
+		return a < b ? a : b;
+	};
+	/// Returns greater integer.
+	int imax(int a, int b) {
+		return a > b ? a : b;
+	};
+	/// Clamps an integer to an inclusive range.
+	int iclamp(int x, int lower, int upper) {
+		return imin(imax(x, lower), upper);
+	};
+
+	/// Returns smaller float.
+	float fmin(float a, float b) {
+		return a < b ? a : b;
+	};
+	/// Returns greater float.
+	float fmax(float a, float b) {
+		return a > b ? a : b;
+	};
+	/// Clamps a float to an inclusive range.
+	float fclamp(float x, float lower, float upper) {
+		return fmin(fmax(x, lower), upper);
+	};
+};
