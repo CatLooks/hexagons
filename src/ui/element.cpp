265ac8363cc7631baf2a4363c47a474b1a96d8d3
CatLooks <catlooksoff@gmail.com>
2025-11-19 18:41:50 +0100

    fixed recalculation ignoring system elements

--- src/ui/element.cpp
+++ src/ui/element.cpp
@@ -214,7 +214,7 @@ namespace ui {
 		_innerRect = padding.apply(_rect);
 
 		// recalculate children
-		for (const auto& element : _elements)
+		for (const auto& element : *this)
 			element->recalculate();
 	};
 
